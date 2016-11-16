58935362addcf471628b9f8e3e30cd4f75981b84
Juan A Cortes <juan.cortes@gmail.com>
2016-11-16 10:10:35 -0500

    Step 5: Movie Search AP - Make a call to the movie search API to return all movies that match a particular text query.

--- movie_database/js/movie-database.js
+++ movie_database/js/movie-database.js
@@ -3,6 +3,7 @@ var app = angular.module("movie-database", ["ui.router"]);
 app.factory("MovieService", function($http) {
   var service = {};
   var API_KEY = "06700781243c60769490b4e1bc29685c";
+
   service.nowPlaying = function() {
     var url = "http://api.themoviedb.org/3/movie/now_playing";
     return $http({
@@ -19,6 +20,18 @@ app.factory("MovieService", function($http) {
       method: "GET",
       url: url,
       params: { api_key: API_KEY }
+    });
+  };
+
+  service.movieSearch = function(queryText) {
+    var url = "http://api.themoviedb.org/3/search/movie";
+    return $http({
+      method: "GET",
+      url: url,
+      params: {
+        api_key: API_KEY,
+        query: queryText
+       }
     });
   };
 
@@ -42,6 +55,17 @@ app.controller("MainController", function($scope, MovieService) {
       // movie details
       $scope.api_results = movieDetails;
       $scope.movie_details = $scope.api_results;
+    });
+  };
+
+  var queryText = $scope.queryText;
+  $scope.movieSearch = function(queryText) {
+    console.log("Clicked the movieSearch button");
+    MovieService.movieSearch(queryText).success(function(moviesFound) {
+      // movies found
+      $scope.api_results = moviesFound;
+      $scope.movies = $scope.api_results.results;
+      console.log($scope.movies);
     });
   };
 
